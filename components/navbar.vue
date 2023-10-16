af900fe4c583555bf7358d600c6d7103a1728f00
Loa212 <anobileloris@gmail.com>
2023-10-16 16:12:06 +0200

    navbar

---
+++ components/navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="w-full bg-gray-100 fixed">
+    navbar
+
+    <div v-if="scrolled">this is scrolled</div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  data: () => ({
+    scrolled: false,
+  }),
+  methods: {
+    handleScroll() {
+      this.scrolled = window.scrollY > 56;
+    },
+  },
+  beforeMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  afterMount() {
+    handleScroll();
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+};
+</script>
