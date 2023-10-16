93ddf8cfc5023c3c2bf48e4bf5e12e0e2697a4a6
Loa212 <anobileloris@gmail.com>
2023-10-16 16:11:41 +0200

    rename main > home

---
+++ components/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="h-full w-full flex flex-col">
+    <h1>Hello</h1>
+
+    <div class="bg-green-700 w-full h-96"></div>
+    <div class="bg-red-700 w-full h-96"></div>
+    <div class="bg-pink-700 w-full h-96"></div>
+    <div class="bg-blue-700 w-full h-96"></div>
+  </main>
+</template>
