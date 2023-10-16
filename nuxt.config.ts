51dd8254c938b1b32919ec9c112ccdb6e90faba3
Loa212 <anobileloris@gmail.com>
2023-10-16 18:54:30 +0200

    add vue-360

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: ["~/assets/css/main.css", "vue-360/dist/css/style.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
