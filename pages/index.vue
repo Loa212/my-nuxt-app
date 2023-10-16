03ed9b4fec93cce990f082c920371675d4eef091
Loa212 <anobileloris@gmail.com>
2023-10-16 15:40:41 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <v-app>
+      <transition name="slide-fade" mode="out-in">
+        <router-view>
+          <!-- main view -->
+          <Main></Main>
+        </router-view>
+      </transition>
+    </v-app>
+    <!-- transitions sliding up using transform -->
+    <div
+      class="absolute inset-0 flex justify-center items-center w-full h-full bg-primary transition-transform duration-500 ease-in-out transform"
+      :class="{ 'translate-y-0': loading, '-translate-y-full': !loading }"
+    >
+      <div class="h-[18rem] w-[12rem] rounded-full bg-white relative">
+        <div
+          class="absolute inset-0 flex justify-center items-center w-full h-full transition-transform duration-500 ease-in-out transform"
+          :class="{ 'translate-y-0': circle, 'translate-y-[40%]': !circle }"
+        >
+          <div
+            class="h-full w-full rounded-full bg-primary ring ring-primary"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data: () => ({
+    loading: true,
+    circle: true,
+  }),
+  mounted() {
+    // lower the circle
+    setTimeout(() => {
+      this.circle = false;
+    }, 1500);
+
+    // hide the loader
+    setTimeout(() => {
+      this.loading = false;
+    }, 2500);
+  },
+};
+</script>
