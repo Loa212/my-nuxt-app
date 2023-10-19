e3ae328c32b73eb1c44ab18a9ef7a4c81fa24d33
Loa212 <anobileloris@gmail.com>
2023-10-19 14:41:24 +0200

    archCoin

---
+++ components/ArchCoin.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-img
+    id="token"
+    width="500"
+    height="500"
+    class="h-[300px] object-contain absolute top-0 right-0 mt-[80px] mr-[320px]"
+    src="../public/ArchCoin/ArchCoin_180_00000.png"
+    alt="archcoin"
+  />
+</template>
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      frameUri: "../public/ArchCoin/ArchCoin_180_00000.png",
+      frameStart: 0,
+      frameEnd: 39,
+    };
+  },
+  methods: {
+    getImgUri() {
+      return require("../public/ArchCoin/ArchCoin_180_00000.png");
+      //   return (
+      //     "../public/ArchCoin/ArchCoin_180_" +
+      //     frame.toString().padStart(5, "0") +
+      //     ".png"
+      //   );
+    },
+  },
+  mounted() {
+    // setup
+    var viewer = document.getElementById("token");
+  },
+};
+</script> -->
--- components/home.vue
+++ components/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="h-full w-full flex flex-col">
+  <main class="relative h-full w-full flex flex-col overflow-x-clip">
     <div class="min-h-[12rem]"></div>
 
     <div class="relative max-w-7xl w-full mx-auto h-96 px-8 z-10">
@@ -15,15 +15,7 @@
       </h1>
     </div>
     <div class="absolute right-0 lg:-mr-[380px] xl:-mr-[100px] z-0">
-      <canvas
-        id="token-animation-3"
-        class="h-[300px] absolute top-0 right-0 mt-[80px] mr-[420px]"
-        width="500"
-        height="500"
-      ></canvas>
-      <div
-        class="absolute left-0 inset-y-0 w-[478px] bg-[linear-gradient(90deg,#F2EFED_39%,rgba(242,239,237,0)_100%)]"
-      ></div>
+      <ArchCoin />
       <img
         class="mx-auto max-w-[1206px]"
         src="https://archway.io/_nuxt/illustration-50.b39a23b0.png"
@@ -35,16 +27,3 @@
     <div class="w-full h-96"></div>
   </main>
 </template>
-<!-- 
-<script>
-import VueThreeSixty from "vue-360";
-
-import "vue-360/dist/css/style.css";
-
-export default {
-  name: "360viewer",
-  mounted() {
-    Vue.use(VueThreeSixty);
-  },
-};
-</script> -->
